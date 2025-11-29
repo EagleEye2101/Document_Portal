625f033facadcfc743201a11bfa557448b758725
EagleEye2101 <karan.raka2003@gmail.com>
2025-11-28 22:20:38 -0500

    doc analyzer

---
+++ src/document_analyser/data_analysis.py
@@ -0,0 +1,14 @@
+import os
+from utils.model_loader import ModelLoader
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from model.models import *
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+
+class DocumentAnalyser:
+    """ Analyzes documents using a pre-trained model. Automatially logs all actions and supports session -based organization."""
+    def __init__(self):
+        pass
+    def analyze_document(self):
+        pass
---
+++ src/document_analyser/data_ingestion.py
@@ -0,0 +1,15 @@
+import os
+import fitz  # PyMuPDF
+import uuid
+from datetime import datetime
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+
+class DocumentHandler:
+    """ Haandle PDF saving and reading operations. Automatically log all actions and suports session-based organization. """
+    def __init__(self):
+        pass
+    def save_pdf(self):
+        pass
+    def read_pdf(self):
+        pass
