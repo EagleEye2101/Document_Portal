f8e4f4e679e646cf88862dce6420d27f890affd1
EagleEye2101 <karan.raka2003@gmail.com>
2025-11-28 22:38:56 -0500

    session id

--- src/document_analyser/data_ingestion.py
+++ src/document_analyser/data_ingestion.py
@@ -7,9 +7,41 @@ from exception.custom_exception import DocumentPortalException
 
 class DocumentHandler:
     """ Haandle PDF saving and reading operations. Automatically log all actions and suports session-based organization. """
-    def __init__(self):
-        pass
+    def __init__(self,data_dir=None,session_id=None):
+        try:
+            self.log=CustomLogger().get_logger(__name__)
+            self.data_dir = data_dir or os.getenv(
+                "DATA_STORAGE_PATH",
+                os.path.join(os.getcwd(), "data", "document_analysis")
+                )
+            self.session_id = session_id or f"session_{datetime.now().strftime('%Y%m%d_%H%M%S')}_{str(uuid.uuid4())[:8]}"
+
+            # Create base session directory if it doesn't exist
+            self.session_path = os.path.join(self.data_dir, self.session_id)
+            os.makedirs(self.session_path, exist_ok=True)
+            self.log.info("PDFHandler initialized", session_id=self.session_id, session_path=self.session_path)
+        except Exception as e:
+            self.log.error("Error initializing DocumentHandler", error=str(e))
+            raise DocumentPortalException("Error initializing DocumentHandler", e) from e
+
+
     def save_pdf(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error("Error saving PDF", error=str(e))
+            raise DocumentPortalException("Error saving PDF", e) from e
     def read_pdf(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error("Error reading PDF", error=str(e))
+            raise DocumentPortalException("Error reading PDF", e) from e
+        
+#test the code 
+if __name__ == "__main__":
+    handler = DocumentHandler()
+   # handler.save_pdf()
+   # handler.read_pdf()
+    print(f"Session ID: {handler.session_id}")
+    print(f"Session Path: {handler.session_path}")
