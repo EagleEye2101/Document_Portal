2a36d485f6b5c6117a66727d2a150126cbd2bc52
EagleEye2101 <karan.raka2003@gmail.com>
2025-11-30 20:01:47 -0500

    prompt template and sesison handling

--- model/models.py
+++ model/models.py
@@ -1,4 +1,15 @@
 from pydantic import BaseModel, Field
-from typing import List, Optional,Dict,Any
+from typing import List, Optional,Dict,Any,Union
 
 #pydantic model added for validation and structure
+
+class Metadata(BaseModel):
+    Sunnary: List[str] = Field(default_factory=list, description="List of summary points extracted from the document")
+    Title:str
+    Author:str
+    DateCreated:str
+    LastModifiedDate:str
+    Publisher:str
+    Language:str
+    PageCount:Union[int,str]
+    SentimentTone:str
--- src/document_analyser/data_analysis.py
+++ src/document_analyser/data_analysis.py
@@ -1,14 +1,30 @@
 import os
+import sys
 from utils.model_loader import ModelLoader
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 from model.models import *
 from langchain_core.output_parsers import JsonOutputParser
 from langchain.output_parsers import OutputFixingParser
+from prompt.prompt_library import *
 
 class DocumentAnalyser:
     """ Analyzes documents using a pre-trained model. Automatially logs all actions and supports session -based organization."""
     def __init__(self):
-        pass
+        try:
+            self.loader= ModelLoader()
+            self.llm=self.loader.load_llm
+
+            # prepare parsers
+            self.parser=JsonOutputParser(pydantic_object=Metadata) #pydantic model for structured output
+            self.fixing_parser= OutputFixingParser.from_llm(llm=self.llm,parser=self.parser)
+
+            self.prompt=prompt
+            self.log.info("DocumentAnalyser initialized successfully.")
+
+        except Exception as e:
+            self.log.error("Error initializing DocumentAnalyser", error=str(e))
+            raise DocumentPortalException("Error initializing DocumentAnalyser", sys)
+
     def analyze_document(self):
         pass
